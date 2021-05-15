06187ee26da5973ee9bc008dd24d74dd88e4c3a5
rovonov98 <stariyakk121@gmail.com>
2021-05-15 19:12:55 +0500

    done

--- client/static/index.js
+++ client/static/index.js
@@ -4,6 +4,15 @@ import AddLogic  from "./Logic/Logic.js"
 
 const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$")
 
+const addSpa = () => {
+  document.body.addEventListener("click", e => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault()
+      navigateTo(e.target.href)
+    }
+  })
+}
+
 const getParams = match => {
   const values = match.result.slice(1)
   const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1])
@@ -42,17 +51,13 @@ const router = async () => {
   const view = new match.route.view(getParams(match))
 
   document.querySelector("#app").innerHTML = await view.getHtml()
+  addSpa()
   AddLogic(match.route.path, getParams(match))
 }
 
 window.addEventListener("popstate", router)
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.addEventListener("click", e => {
-    if (e.target.matches("[data-link]")) {
-      e.preventDefault()
-      navigateTo(e.target.href)
-    }
-  })
+  addSpa()
   router()
 })
